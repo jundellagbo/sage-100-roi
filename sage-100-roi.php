84abbdfb09ea3f0a4f8332e46125727d04b579d6
jundellagbo <jundellagbo10dd@gmail.com>
2026-03-14 01:38:17 +0800

    version: 4.0.9

--- sage-100-roi.php
+++ sage-100-roi.php
@@ -3,7 +3,7 @@
  * Plugin Name: Sage API Connection to ROI Consulting
  * Plugin URI: mailto:jj@xooker.com
  * Description: Connect and sync ecommerce platform from SAGE 100 to Wordpress Woocommerce
- * Version: 4.0.8
+ * Version: 4.0.9
  * Author: JJXooker
  * Author URI: mailto:jj@xooker.com
  * License: GPL-2.0+
